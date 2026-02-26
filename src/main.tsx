856069fd8bcb133859040777c74e8e9f5557580d
Beckerziin <joao_becker7@hotmail.com>
2026-02-25 21:48:18 -0300

    merge das 2 versao

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Calculadora from './Pages/Calculadora.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Calculadora />
+  </StrictMode>,
+)
+
+
 class Dinheiro {
   private centavos: number;
 
